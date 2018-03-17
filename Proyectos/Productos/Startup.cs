0ef99df8fc0b38d3b695054b545e990cb225c2a0
Starling Germosen <sgrysoft@outlook.com>
2018-03-17 12:52:23 -0400

    Creacion del proyecto

---
+++ Proyectos/Productos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Productos.Startup))]
+namespace Productos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
